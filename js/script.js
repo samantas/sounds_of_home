c959647ca425eff9a13e243d9922e37fac81a619
samantas <samantas@nyu.edu>
2019-03-31 12:29:21 +0700

    newest audio files, pause and play btn, final word map

--- js/script.js
+++ js/script.js
@@ -92,17 +92,69 @@ class UserStory extends Audio {
 
 let currentAudio = new UserStory();
 const audioTracks = [
-    new UserStory('audio/home_1.wav', [
-        'So my home sounds to me like the footsteps of my family members, which are very, very distinct.',
-        'I can just hear the footsteps of either my brother, my dad, or my mom, and I know exactly which ones belong to which family member.',
-        'My home sounds to me also like just something happening in the kitchen. And based on the sounds coming from the kitchen I know who is in the kitchen.',
-        'And also, the doors being closed or open, and evening depending on that sound I know who did that activity.'
+    new UserStory('audio/new/WDHSLTY_1.mp3', [
+        'my bad,...uh, my dog',
+        'and with like my laptop open on my lap playing like Narso',
+        'thats it'
     ], [
-        'footsteps of my brother',
-        'the kitchen',
-        'the doors'
+        'my dog',
+        'laptop'
     ]),
-    new UserStory('audio/home_2.wav', [
+    new UserStory('audio/new/WDHSLTY_2.mp3', [
+        'umm, home usually sounds like roosters, to be honest, and then cows giving birth',
+        'and then my dad typing on his computer',
+        'and then, usually like home movies',
+        'or like, any type of like cooking channels on, so yeah'
+    ], [
+        'roosters',
+        'cows giving birth',
+        'cooking channels'
+    ]),
+    new UserStory('audio/new/WDHSLTY_3.mp3', [
+        'moooomy, moomy'
+    ], [
+        'mooommy'
+    ]),
+    new UserStory('audio/new/WDHSLTY_4.mp3', [
+        'home sounds like dogs barking, my mom yelling at me *giggles*',
+        'umm, my family on a shrimp farm (cant tell what she is saying?)',
+        'my dogs, and us eating'
+    ], [
+        'dogs barking',
+        'my mom yelling',
+        'my dogs'
+    ]),
+    new UserStory('audio/new/WDHSLTY_5.mp3', [
+        'I guess home to me sounds like the sound of everybody doing their own thing cuz my family is pretty independent',
+        'whether its my mom cooking or watching yoga videos on youtube',
+        'umm, or my brother watching video game videos on youtube, or playing his nintendo switch game',
+        'home to me just sounds like everybody being in their own zone and doing whatever they want, basically'
+    ], [
+        'everybody doing their own thing',
+        'mom cooking',
+        'yoga videos',
+        'nintendo switch'
+    ]),
+    new UserStory('audio/new/WDHSLTY_6.mp3', [
+        'ok, uh, home to me sounds like, umm, family dinners',
+        'late night drives with my friends with the volume turned all the way up on the radio',
+        'waves crashing, crackling of a bonfire, yeah thats what home sounds like to me'
+    ], [
+        'family dinners',
+        'late night drives',
+        'radio',
+        'waves crashing',
+        'crackling of a bonfire'
+    ]),
+    new UserStory('audio/new/WDHSLTY_7.mp3', [
+        'I guess to me home sounds like, hoooo...m...e, so I hear the h and the o sound but the m part doesnt register as much',
+        'so theres an emphasis on the h, and the most emphasis on the o, and then not so much emphasis on the m',
+        '(interviewer: why is that?)',
+        'I dont know, hoooo..., thats the most important part'
+    ], [
+        'hoooo...m..e'
+    ]),
+    new UserStory('audio/new/WDHSLTY_8.mp3', [
         'I am pulling up a song, so I am from a small town just outside of New Orleans...ummmm',
         'and, uh, this is a song my dad played a ton whenever I was...just, throughout my whole life',
         'uh, it is called backwater bayou, it is from a group from New Orleans, and it is just instrumental',
@@ -118,70 +170,43 @@ const audioTracks = [
         'song my dad played',
         'backwater bayou'
     ]),
-    // new UserStory('audio/What does home sound like - part 3.m4a', [], ['word map text']),
-    // new UserStory('audio/What does home sound like - part 4.m4a', [], ['word map text']),
-    // new UserStory('audio/What does home sound like - part 6.m4a', [], ['word map text']),
-    new UserStory('audio/WDHSLTY_1.mp3', [
-        'my bad,...uh, my dog',
-        'and with like my laptop open on my lap playing like Narso',
-        'thats it'
+    new UserStory('audio/new/WDHSLTY_9.mp3', [
+        'umm, so I grew up a lot around Latino music, and theres a song that my mom likes to play a lot',
+        'and shes played it since, shes been playing it for a while now so it really hits close to home',
+        'and umm here you go, its called Despacito'
     ], [
-        'my dog',
-        'laptop'
+        'Latino music',
+        'Despacito'
     ]),
-    new UserStory('audio/WDHSLTY_2.mp3', [
-        'umm, home usually sounds like roosters, to be honest, and then cows giving birth',
-        'and then my dad typing on his computer',
-        'and then, usually like home movies',
-        'or like, any type of like cooking channels on, so yeah'
+    new UserStory('audio/new/WDHSLTY_10.mp3', [
+        'uh, I guess, well whne I think of the word I think of the way it sounds',
+        'so hoooo...mm. So I guess its more of a round feeling when I say the o sound',
+        'and I guess I can equate that to kind of like when you are home you are in a circle',
+        'and then you can equate that to your family, and the warmth you get from your family',
+        'so I think that is what home sounds like to me'
     ], [
-        'roosters',
-        'cows giving birth',
-        'cooking channels'
+        'hoooom...mm',
+        'round feeling',
+        'warmth'
     ]),
-    new UserStory('audio/WDHSLTY_3.mp3', [
-        'moooomy, moomy'
+    new UserStory('audio/new/WDHSLTY_11.mp3', [
+        'So my home sounds to me like the footsteps of my family members, which are very, very distinct.',
+        'I can just hear the footsteps of either my brother, my dad, or my mom, and I know exactly which ones belong to which family member.',
+        'My home sounds to me also like just something happening in the kitchen. And based on the sounds coming from the kitchen I know who is in the kitchen.',
+        'And also, the doors being closed or open, and evening depending on that sound I know who did that activity.'
     ], [
-        'mooommy'
+        'footsteps of my brother',
+        'the kitchen',
+        'the doors'
     ]),
-    new UserStory('audio/WDHSLTY_4.mp3', [
-        'home sounds like dogs barking, my mom yelling at me *giggles*',
-        'umm, my family on a shrimp farm (cant tell what she is saying?)',
-        'my dogs, and us eating'
+    new UserStory('audio/new/WDHSLTY_12.mp3', [
+        'home? I dont know, a place, and with my cats',
+        'umm, with my family, and a place where I feel safe',
+        'thats it i dont know do you need something else?'
     ], [
-        'dogs barking',
-        'my mom yelling',
-        'my dogs'
-    ]),
-    new UserStory('audio/WDHSLTY_5.mp3', [
-        'I guess home to me sounds like the sound of everybody doing their own thing cuz my family is pretty independent',
-        'whether its my mom cooking or watching yoga videos on youtube',
-        'umm, or my brother watching video game videos on youtube, or playing his nintendo switch game',
-        'home to me just sounds like everybody being in their own zone and doing whatever they want, basically'
-    ], [
-        'everybody doing their own thing',
-        'mom cooking',
-        'yoga videos',
-        'nintendo switch'
-    ]),
-    new UserStory('audio/WDHSLTY_6.mp3', [
-        'ok, uh, home to me sounds like, umm, family dinners',
-        'late night drives with my friends with the volume turned all the way up on the radio',
-        'waves crashing, crackling of a bonfire, yeah thats what home sounds like to me'
-    ], [
-        'family dinners',
-        'late night drives',
-        'radio',
-        'waves crashing',
-        'crackling of a bonfire'
-    ]),
-    new UserStory('audio/WDHSLTY_7.mp3', [
-        'I guess to me home sounds like, hoooo...m...e, so I hear the h and the o sound but the m part doesnt register as much',
-        'so theres an emphasis on the h, and the most emphasis on the o, and then not so much emphasis on the m',
-        '(interviewer: why is that?)',
-        'I dont know, hoooo..., thats the most important part'
-    ], [
-        'hooo...m..e'
+        'cats',
+        'family',
+        'safe'
     ]),
 ];
 
